be896e007747c207a0065dd04e8241c523b47bb7
samuel <samuelaragon616@gmail.com>
2024-11-20 13:17:28 -0300

    update layout for mobile devices

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Samuel Aragón",
-  description: "Portofolio Samuel Aragón",
+  description: "Portfolio Samuel Aragón",
 };
 
 export default function RootLayout({
@@ -13,6 +13,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
