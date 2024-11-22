1e50d4d3d9d21f4cf336e660596aa41278b59741
samuel <samuelaragon616@gmail.com>
2024-11-21 21:42:38 -0300

    update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,16 @@ import Navigation from "@/components/Navigation";
 import React from "react";
 
 function AboutPage() {
-  return <Navigation />;
+  return (
+    <>
+      <div className="h-screen w-screen">
+        <Navigation />
+        <div>
+          <h4>Un poco mas sobre mi</h4>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default AboutPage;
